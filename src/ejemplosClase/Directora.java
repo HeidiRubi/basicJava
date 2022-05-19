23ef5efe68dcf0dea89389c0f3c06ec86df0c7cc
HeidiRubi <104862565+HeidiRubi@users.noreply.github.com>
2022-05-18 19:49:42 -0500

    Add files via upload

---
+++ src/ejemplosClase/Directora.java
@@ -0,0 +1,25 @@
+package ejemplosClase;
+
+public class Directora {
+    String nombre;
+    int edad;
+    int añosEJercidos;
+    String nomEscuela;
+    String estado;//soltera, casada, etc.
+
+    public void tareas(){
+
+    }
+
+    public void recesos(){
+
+    }
+
+    public  void juntas(){
+
+    }
+
+    public void salida(){
+
+    }
+}

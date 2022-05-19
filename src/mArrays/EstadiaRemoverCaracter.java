993118089ce61407f869f2c3914ad2a0a1562f05
HeidiRubi <104862565+HeidiRubi@users.noreply.github.com>
2022-05-18 19:55:41 -0500

    Add files via upload

---
+++ src/mArrays/EstadiaRemoverCaracter.java
@@ -0,0 +1,31 @@
+package mArrays;
+
+import libs.Input;
+
+
+public class EstadiaRemoverCaracter {
+    //Escriba un programa que remueva cierto caracter de una cadena dada.
+    //
+    //Por ejemplo
+    //
+    //s = "ABCAAADEAFA";
+    //t = strip(s, 'A');  // "BCDEF"
+    //
+    //s = "ABCDEFG";
+    //t = strip(s, 'W') // "ABCDEFG"
+    public static void main(String[] args) {
+        String cadena = Input.get_string("Escribe ciertas letras en mayuscula: ");
+        int eliminarLetra = Input.get_int("Escribe la letra que deseas eliminar");
+        eliminarLetra = Integer.parseInt(String.valueOf(0));
+       // Input.print(eliminarLetra.trim()+);Input.print(c.trim() + "\n");
+        //for(int contadorL = 0; contadorL < eliminarLetra; contadorL++)
+
+
+       // System.out.println("Su texto quedo asi: "+ eliminarLetra.trim());
+
+
+        }
+
+
+    }
+
